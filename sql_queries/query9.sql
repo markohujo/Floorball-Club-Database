09c40ca7205191fee8aceff6878e02beb75d558a
Marko Hujo <hujomark@fit.cvut.cz>
2021-04-19 23:14:42 +0200

    add query

---
+++ sql_queries/query9.sql
@@ -0,0 +1,10 @@
+
+select c.email,
+       c.meno,
+       c.priezvisko
+from clen c join trener using (id_clen)
+where (
+    select extract (year from age(datum_narodenia)) < 25
+          );
+
+-- Tréneri (email, meno, priezvisko), ktorí majú menej ako 25 rokov.
